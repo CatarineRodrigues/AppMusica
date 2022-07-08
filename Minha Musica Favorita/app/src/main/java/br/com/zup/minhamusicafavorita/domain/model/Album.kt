5bb2968ad280b6f068495796aa21a2d704074d1d
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-08 00:02:50 -0300

    Criar interface DAO

--- Minha Musica Favorita/app/src/main/java/br/com/zup/minhamusicafavorita/domain/model/Album.kt
+++ Minha Musica Favorita/app/src/main/java/br/com/zup/minhamusicafavorita/domain/model/Album.kt
@@ -1,19 +1,48 @@
 package br.com.zup.minhamusicafavorita.domain.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class Album(
+@Entity(tableName = "albuns")
+data class Album(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "cod_album")
+    var codAlbum: Long = 1,
+
+    @ColumnInfo(name = "imagem")
     private var imagem: Int,
+
+    @ColumnInfo(name = "album")
     private var nomeAlbum: String,
+
+    @ColumnInfo(name = "descricao")
     private var descricaoAlbum: String,
+
+    @ColumnInfo(name = "artista")
     private var artista: String,
+
+    @ColumnInfo(name = "lancamento")
     private var lancamento: Int,
+
+    @ColumnInfo(name = "gravadora")
     private var gravadora: String,
+
+    @ColumnInfo(name = "estudio")
     private var estudio: String,
+
+    @ColumnInfo(name = "formato")
     private var formato: String,
-    private var genero: String
+
+    @ColumnInfo(name = "genero")
+    private var genero: String,
+
+    @ColumnInfo(name = "favoritado")
+    var isFavorite: Boolean = false
+
 ) : Parcelable {
     fun getImagem() = this.imagem
     fun getNomeAlbum() = this.nomeAlbum
